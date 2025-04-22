58b0c3beeb6936a34c7545c47ee4b2cd346dcfb8
dinaahmed10 <codedinaaa@gmail.com>
2025-04-22 17:25:19 +0200

    final solve

--- src/problems/if_else/FindGreatestAmongThreeNumbers.java
+++ src/problems/if_else/FindGreatestAmongThreeNumbers.java
@@ -1,3 +1,16 @@
+// https://www.w3resource.com/java-exercises/conditional-statement/index.php
+
+//3. Find Greatest Among Three Numbers
+//
+//        Write a Java program that takes three numbers from the user and prints the greatest number.
+//
+//        Test Data
+//        Input the 1st number: 25
+//        Input the 2nd number: 78
+//        Input the 3rd number: 87
+//        Expected Output :
+//        The greatest: 87
+
 package problems.if_else;
 
 import java.util.Scanner;
@@ -13,10 +26,8 @@ public class FindGreatestAmongThreeNumbers {
         int Number3= input.nextInt();
 
         int greatest = Number1;
-        if (Number2 > greatest)
-            greatest = Number2;
-        if (Number3 > greatest)
-            greatest = Number3;
+        if (Number2 > greatest) greatest = Number2;
+        if (Number3 > greatest) greatest = Number3;
 
         System.out.println("The greatest number is: " + greatest);
     }
